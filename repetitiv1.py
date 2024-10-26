714493db4460d33ac1c70aa3fe071d8713d937b0
TalkingTommy28 <denis.nastase07@gmail.com>
2024-10-26 13:28:21 +0300

    Primele Probleme

---
+++ repetitiv1.py
@@ -0,0 +1,5 @@
+a=int(input())
+
+for i in range (2,a+1) :
+    if i%2 == 0 :
+        print(i, end=" ")
---
+++ repetitiv2.py
@@ -0,0 +1,11 @@
+a=int(input())
+k=0
+while a!=0:
+    if a%2 == 0:
+        k=k+1
+  
+    a=int(input())
+if k==0 :
+    print("NU EXISTA")
+else :
+    print(k)
